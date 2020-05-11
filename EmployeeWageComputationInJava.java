64810f22826995ea44f3a0a9ee236721dac749c0
Sudhanshu Ghinmine <sghinmine54@gmail.com>
2020-05-11 08:05:45 +0530

    Employee Wage computation start in java

---
+++ EmployeeWageComputationInJava.java
@@ -0,0 +1,5 @@
+public class EmployeeWageComputationInJava {
+    public static void main(String[] args){
+        System.out.println("Welcome to employee wage computation program on master branch");
+    }
+}
