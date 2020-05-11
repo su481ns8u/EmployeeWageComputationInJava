02c0d55133a98c50af40e138b27359d03389042c
Sudhanshu Ghinmine <sghinmine54@gmail.com>
2020-05-11 14:55:27 +0530

    Wages till condition for total work hrs or days not reached

--- EmployeeWageComputationInJava.java
+++ EmployeeWageComputationInJava.java
@@ -8,23 +8,28 @@ class calculateWage {
 class checkAttendance extends calculateWage{
     int check;
     static int days = 0;
+    static int workHrs = 0;
+    static int totalWorkHrs = 0;
     void isPresent(){
         int wage = 0;
         int totalWage = 0;
-        while ( days != 20 ){
+        while ( days != 20 && totalWorkHrs <= 100 ) {
             days++;
+            System.out.println(days);
             check = (int)(Math.floor(Math.random() * 10) % 3);
             switch(check) {
                 case 0:
-                    wage = dailyWage(8);
+                    workHrs = 8;
                     break;
                 case 1:
-                    wage = dailyWage(4);
+                    workHrs = 4;
                     break;
                 default:
-                    wage = 0;
+                    workHrs = 0;
                     break;
             }
+            totalWorkHrs = totalWorkHrs + workHrs;
+            wage = dailyWage(workHrs);
             totalWage = totalWage + wage;
         }
         System.out.println("Total earning of employee for a month is "+totalWage);
