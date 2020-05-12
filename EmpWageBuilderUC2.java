4c03d7ad88ee7f204951233fc7c0c6b99d87f8ac
Sudhanshu Ghinmine <sghinmine54@gmail.com>
2020-05-12 15:44:32 +0530

    Calculate Daily employee wage

---
+++ EmpWageBuilderUC2.java
@@ -0,0 +1,22 @@
+public class EmpWageBuilderUC2 {
+    public static void main(String args[]){
+        //constants
+        int IS_FULL_TIME = 1;
+        int EMP_RATE_PER_HR = 20;
+
+        //VARIABLES
+        int empHrs = 0;
+        int empWage = 0;
+
+        //COMPUTATION
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if(empCheck == IS_FULL_TIME){
+            empHrs = 8;
+        }
+        else{
+            empHrs = 0;
+        }
+        empWage = empHrs * EMP_RATE_PER_HR;
+        System.out.println("Employee Wage: "+empWage);
+    }
+}
