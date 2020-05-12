21dd032902658d09ddce247c6090d611c819d623
Sudhanshu Ghinmine <sghinmine54@gmail.com>
2020-05-12 15:43:31 +0530

    Checked the presenty of employee

---
+++ EmpWageBuilderUC1.java
@@ -0,0 +1,15 @@
+public class EmpWageBuilderUC1 {
+    public static void main(String args[]){
+        //constants
+        int IS_FULL_TIME = 1;
+
+        //COMPUTATION
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if(empCheck == IS_FULL_TIME){
+            System.out.println("Employee is Present");
+        }
+        else{
+            System.out.println("Employee is Absent");
+        }
+    }
+}
