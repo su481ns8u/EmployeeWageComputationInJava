7d932e0e6d15f0c9a5f85ec2e9e6ae5a0b2e1e13
Sudhanshu Ghinmine <sghinmine54@gmail.com>
2020-05-13 09:07:26 +0530

    Refactored code o write a class method to compute employee wage

---
+++ EmpWageBuilderClassMethod.java
@@ -0,0 +1,40 @@
+public class EmpWageBuilderClassMethod {
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
+
+    public static final int EMP_RATE_PER_HR = 20;
+    public static final int NUM_OF_WORKING_DAYS = 20;
+    public static final int MAX_HRS_IN_MONTH = 100;
+
+    static int totalEmpWage() {
+        //VARIABLES
+        int empHrs = 0;
+        int totalEmpHrs = 0;
+        int totalWorkingDays = 0;
+
+        //COMPUTATION
+        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+            totalWorkingDays++;
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+            }
+            totalEmpHrs += empHrs;
+            System.out.println("Days: " + totalWorkingDays + " Emp Hours: " + empHrs);
+        }
+        return totalEmpHrs * EMP_RATE_PER_HR;
+    }
+
+    public static void main(String args[]) {
+        int totalEmpWage = totalEmpWage();
+        System.out.println("Total Emp Wage: " + totalEmpWage);
+    }
+}
